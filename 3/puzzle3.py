bf6b46a38f1d74c60679d9bbd7d13594ec9b3d45
kimo-s <karim.ahmed1999@hotmail.com>
2022-04-08 01:09:10 -0400

    Solution to puzzle 3 part 2 added

--- 3/puzzle3.py
+++ 3/puzzle3.py
@@ -3,9 +3,10 @@
 f = open("input.txt", "r")
 
 buf = f.readline()
+input = []
 total = 0
 
-gamma_rate = [0 for x in range(len(buf))]
+freqcancy = [0 for x in range(len(buf))]
 
 while buf != '':
     total += 1
@@ -13,15 +14,103 @@ while buf != '':
     if '\n' in buf:
         buf.remove('\n')
     
-    gamma_rate = list(map(lambda x, y: int(x)+y, buf, gamma_rate))
+    freqcancy = list(map(lambda x, y: int(x)+y, buf, freqcancy))
 
+    input.append(buf)
     buf = f.readline()
 
 
-epsilon_rate = ['1' if x < total/2 else '0' for x in gamma_rate]
-gamma_rate = ['1' if x > total/2 else '0' for x in gamma_rate]
+epsilon_rate = ['1' if x < total/2 else '0' for x in freqcancy]
+gamma_rate = ['1' if x > total/2 else '0' for x in freqcancy]
 
 gamma_rate = eval(f"0b{''.join(gamma_rate)}")
 epsilon_rate = eval(f"0b{''.join(epsilon_rate)}")
 
-print(gamma_rate*epsilon_rate)
+print(f"Gamma rate {gamma_rate} Epsilon {epsilon_rate}. Result {gamma_rate*epsilon_rate}")
+
+
+oxygan_rate = []
+
+size = len(input[0])
+readInput = input.copy()
+
+for i in range(size):
+    freq = 0
+    for s in range(len(input)):
+        if input[s] != 0:
+            if int(input[s][i]) == 1:
+                freq += 1
+
+    temp = 0
+    for ss in range(len(input)):
+        if input[ss] != 0:
+            temp += 1
+
+    for q in range(len(input)):
+        if input[q] == 0:
+            continue
+        if freq >= temp/2:
+            if int(input[q][i]) == 0:
+                input[q] = 0
+        else:
+            if int(input[q][i]) == 1:
+                input[q] = 0
+
+    temp = 0
+    for ss in range(len(input)):
+        if input[ss] != 0:
+            temp += 1
+
+    if temp == 1:
+        for q in range(len(input)):
+            if input[q] != 0:
+                oxygan_rate = input[q]
+                break
+        break
+     
+
+input = readInput
+
+CO2_rating = []
+
+for i in range(size):
+    freq = 0
+    for s in range(len(input)):
+        if input[s] != 0:
+            if int(input[s][i]) == 1:
+                freq += 1
+
+    temp = 0
+    for ss in range(len(input)):
+        if input[ss] != 0:
+            temp += 1
+
+    for q in range(len(input)):
+        if input[q] == 0:
+            continue
+        if freq >= temp/2:
+            if int(input[q][i]) == 1:
+                input[q] = 0
+        else:
+            if int(input[q][i]) == 0:
+                input[q] = 0
+    
+    temp = 0
+    for ss in range(len(input)):
+        if input[ss] != 0:
+            temp += 1
+
+    if temp == 1:
+        for q in range(len(input)):
+            if input[q] != 0:
+                CO2_rating = input[q]
+                break
+        break
+
+
+
+
+oxygan_rate = eval(f"0b{''.join(oxygan_rate)}")
+CO2_rating = eval(f"0b{''.join(CO2_rating)}")
+
+print(f"Oxygen {oxygan_rate} and CO2 {CO2_rating}. Answer: {oxygan_rate*CO2_rating}")
